4fa51b6d4f58ea5a2dddf4745c8de6764d86d415
hamgeek <ham.intl.id@gmail.com>
2024-08-24 11:24:30 +0700

    feat: add workspace home page

---
+++ src/app/(root)/workspace/home/page.tsx
@@ -0,0 +1,9 @@
+import { WorkspaceHome } from '@/views/workspace-home';
+
+export default async function () {
+  return (
+    <div className="w-full p-8">
+      <WorkspaceHome />
+    </div>
+  );
+}
