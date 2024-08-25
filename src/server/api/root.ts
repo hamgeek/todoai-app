1923fd773ef45fabc67af215022b74699a212309
hamgeek <ham.intl.id@gmail.com>
2024-08-25 23:42:37 +0700

    feat: add todo & task api to root

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,11 +1,16 @@
 import { createCallerFactory, createTRPCRouter } from '@/server/api/trpc';
 
+import { taskRouter, todoRouter } from './routers';
+
 /**
  * This is the primary router for your server.
  *
  * All routers added in /api/routers should be manually added here.
  */
-export const appRouter = createTRPCRouter({});
+export const appRouter = createTRPCRouter({
+  todo: todoRouter,
+  task: taskRouter,
+});
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
