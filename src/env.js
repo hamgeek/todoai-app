2e52aa9f7cfa54e5ee43033c66cedaaff0e8c0ed
hamgeek <ham.intl.id@gmail.com>
2024-08-25 23:45:39 +0700

    feat: add groq api key

--- src/env.js
+++ src/env.js
@@ -25,6 +25,7 @@ export const env = createEnv({
     ),
     GOOGLE_CLIENT_ID: z.string(),
     GOOGLE_CLIENT_SECRET: z.string(),
+    GROQ_APIKEY: z.string(),
   },
 
   /**
@@ -48,6 +49,7 @@ export const env = createEnv({
     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
     GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
     GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+    GROQ_APIKEY: process.env.GROQ_APIKEY,
   },
   /**
    * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
